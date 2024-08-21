03e8fda52cf3f76350ee62ae787d1f42aa63f9e9
JC <137075665+jccruz09@users.noreply.github.com>
2024-08-21 23:44:43 +0800

    Create case_study_1.sql

---
+++ case_study_1.sql
@@ -0,0 +1,41 @@
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+-- 1. What is the total amount each customer spent at the restaurant?
+-- 2. How many days has each customer visited the restaurant?
+-- 3. What was the first item from the menu purchased by each customer?
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+-- 5. Which item was the most popular for each customer?
+-- 6. Which item was purchased first by the customer after they became a member?
+-- 7. Which item was purchased just before the customer became a member?
+-- 8. What is the total items and amount spent for each member before they became a member?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+-- Example Query:
+SELECT
+  	product_id,
+    product_name,
+    price
+FROM dannys_diner.menu
+ORDER BY price DESC
+LIMIT 5;
+
+select 
+	customer_id,
+    sum(price)
+from dannys_diner.sales
+join dannys_diner.menu on dannys_diner.sales.product_id = dannys_diner.menu.product_id
+group by 1
+order by 1
+;
+
+select 
+	customer_id,
+    count (distinct order_date)
+from dannys_diner.sales
+group by 1
+;
+
+-- to be continued
