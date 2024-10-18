1accb2e42cbf57047e1d88abb4f4cf5c43eb7e7c
Julliana Cruz <137075665+jzc0117@users.noreply.github.com>
2024-10-18 15:05:32 +0800

    added #3

--- case_study_2/D PricingsAndRatings.sql
+++ case_study_2/D PricingsAndRatings.sql
@@ -52,6 +52,33 @@ left join pizza_runner.runner_orders as r on r.order_id=c.order_id
  where cancellation_cleaned is null
  ;
 
+
+-- 3. he Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, 
+-- how would you design an additional table for this new dataset - generate a schema for this new table and insert your 
+-- own data for ratings for each successful customer order between 1 to 5.
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+  "review_id" INTEGER,
+  "review_rating" INTEGER,
+  "order_id" INTEGER,
+  "customer_id" INTEGER,
+  "remarks" VARCHAR(255),
+  "review_time" TIMESTAMP
+);
+INSERT INTO reviews
+  ("review_id", "review_rating", "order_id", "customer_id", "remarks","review_time")
+VALUES
+  (1, 5, 1, 101, '', '2020-01-01 19:05:02'),
+  (2, 5, 2, 101, '', '2020-01-01 20:30:01'),
+  (3, 5, 3, 102, '', '2020-01-02 00:20:00'),
+  (4, 4, 4, 103, '', '2020-01-04 14:00:40'),
+  (5, 5, 5, 104, '', '2020-01-08 23:00:20'),
+  (6, 5, 7, 105, '', '2020-01-08 22:00:29'),
+  (7, 4, 8, 102, '', '2020-01-10 00:30:33'),
+  (8, 5, 10, 104, '', '2020-01-11 19:30:00');
+
+
+
 -- 5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is 
 -- 	paid $0.30 per kilometre traveled - how much money does Pizza Runner have left over after these deliveries?
 with cte as (
