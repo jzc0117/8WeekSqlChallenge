eb9e83314659d2cf484c2c803c4a3cb57e5e6f2e
Julliana Cruz <137075665+jzc0117@users.noreply.github.com>
2024-10-18 14:27:44 +0800

    Update D PricingsAndRatings.sql

--- case_study_2/D PricingsAndRatings.sql
+++ case_study_2/D PricingsAndRatings.sql
@@ -1,3 +1,5 @@
+--1. If a Meat Lovers pizza costs $12 and Vegetarian costs $10 and there were no charges for changes 
+-- - how much money has Pizza Runner made so far if there are no delivery fees?
 with cte as (
 select 
 	c.order_id,
@@ -20,4 +22,33 @@ left join pizza_runner.runner_orders as r on r.order_id=c.order_id
  sum(cost)
  from cte
  where cancellation_cleaned is null
- 
+ ;
+
+-- 2. What if there was an additional $1 charge for any pizza extras?
+-- 	Add cheese is $1 extra
+
+with cte as (
+select 
+	c.order_id,
+    c.customer_id,
+    c.pizza_id,
+    c.exclusions,
+    c.extras,
+    c.order_time,
+    case when c.pizza_id=1 then 12
+    else 10
+    end as cost,
+    length(replace(replace(replace(extras, ' ', ''), ',', ''), 'null', '')) as extras_price,
+    case when cancellation = 'Restaurant Cancellation' then 'Restaurant Cancellation'
+  		when cancellation='Customer Cancellation' then 'Customer Cancellation'
+  		else Null
+  end as cancellation_cleaned
+from pizza_runner.customer_orders as c
+left join pizza_runner.runner_orders as r on r.order_id=c.order_id
+  )
+ select 
+ sum(cost)+sum(extras_price)  as total_sum
+ from cte
+ where cancellation_cleaned is null
+ ;
+
