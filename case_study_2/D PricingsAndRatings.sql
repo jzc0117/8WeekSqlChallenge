8d6e7c4d5a8fb061a40aeb87b1b4b66ee8512089
Julliana Cruz <137075665+jzc0117@users.noreply.github.com>
2024-10-18 14:35:15 +0800

    added #5

--- case_study_2/D PricingsAndRatings.sql
+++ case_study_2/D PricingsAndRatings.sql
@@ -52,3 +52,30 @@ left join pizza_runner.runner_orders as r on r.order_id=c.order_id
  where cancellation_cleaned is null
  ;
 
+-- 5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is 
+-- 	paid $0.30 per kilometre traveled - how much money does Pizza Runner have left over after these deliveries?
+with cte as (
+select 
+	c.order_id,
+    c.pizza_id,
+    c.exclusions,
+    c.extras,
+    c.order_time,
+    case when c.pizza_id=1 then 12
+    else 10
+    end as cost,
+    case when cancellation = 'Restaurant Cancellation' then 'Restaurant Cancellation'
+  		when cancellation='Customer Cancellation' then 'Customer Cancellation'
+  		else Null
+  end as cancellation_cleaned,
+  distance,
+  NULLIF(regexp_replace(r.distance, '[^\d.]','','g'), '')::numeric*0.3 as runner_salary
+from pizza_runner.customer_orders as c
+left join pizza_runner.runner_orders as r on r.order_id=c.order_id
+  )
+ select 
+ sum(cost) - sum(runner_salary) as revenue
+ from cte
+ where cancellation_cleaned is null
+ ;
+
